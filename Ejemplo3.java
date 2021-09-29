478a94b231fbde31e6dcdf4951e88c5edcfca133
Alejandro Acosta <90892517+TakierSugar@users.noreply.github.com>
2021-09-29 09:00:05 +0200

    Add files via upload

---
+++ Ejemplo3.java
@@ -0,0 +1,14 @@
+//Un programa que calcula una división y una suma.
+public class Ejemplo3 {
+
+	public static void main(String[] args) {
+	
+		int multiplo = 5;
+		
+
+		
+		System.out.println(multiplo*multiplo);
+		
+	}
+
+}
