4cc7bd8e89d616964601c59c41eb3ed00cb2b36d
Alejandro <takiersugar@gmail.com>
2021-10-04 09:33:57 +0200

    Ejemplo 6 arreglado

--- Ejemplo6.java
+++ Ejemplo6.java
@@ -4,21 +4,20 @@ public class Ejemplo6 {
         double A;
         double Area;
         double Perimetro;
-        double Pi = 3.14159;
+        final double Pi = 3.14159;
         Scanner reader = new Scanner(System.in);
 
         System.out.println("Introduce el radio:");
-        A = reader.nextInt();
+        A = reader.nextDouble();
         
 
        
 
         Area = A * Pi * Pi;
         Perimetro = 2 * Pi * A;
-        System.out.println("El area es:"); 
-        System.out.println(Area);
-        System.out.println("El perimetro es:"); 
-        System.out.println(Perimetro);
+        System.out.println("El area es:" + Area); 
+        System.out.println("El perimetro es:" + Perimetro); 
+     
         
         reader.close();
         
