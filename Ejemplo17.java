3975e34c9bef99cda273e8ba89d85512e7cb79fa
Alejandro <takiersugar@gmail.com>
2021-10-06 09:30:53 +0200

    Puesto e lnombre de los meses en el 17

--- Ejemplo17.java
+++ Ejemplo17.java
@@ -13,18 +13,19 @@ public class Ejemplo17 {
         mes = reader.nextInt();
         reader.close();
         switch (mes){
-            case 1 : System.out.println("31"); break;
-            case 2 : System.out.println("28 o 29"); break;
-            case 3 : System.out.println("31"); break;
-            case 4 : System.out.println("30"); break;
-            case 5 : System.out.println("31"); break;
-            case 6 : System.out.println("30"); break;
-            case 7 : System.out.println("31"); break;
-            case 8 : System.out.println("31"); break;
-            case 9 : System.out.println("30"); break;
-            case 10 : System.out.println("31"); break;
-            case 11 : System.out.println("3o"); break;
-            case 12 : System.out.println("31"); break;
+            case 1 : System.out.println("Enero tiene 31 dias"); break;
+            case 2 : System.out.println("Febrero tiene 28 dias o 29 si es año bisiesto"); break;
+            case 3 : System.out.println("Marzo tiene 31 dias"); break;
+            case 4 : System.out.println("Abril tiene 30 dias"); break;
+            case 5 : System.out.println("Mayo tiene 31 dias"); break;
+            case 6 : System.out.println("Junio tiene 30 dias"); break;
+            case 7 : System.out.println("Julio tiene 31 dias"); break;
+            case 8 : System.out.println("Agosto tiene 31 dias"); break;
+            case 9 : System.out.println("Septiembre tiene 30 dias"); break;
+            case 10 : System.out.println("Octubre tiene 31 dias"); break;
+            case 11 : System.out.println("Noviembre tiene 30 dias"); break;
+            case 12 : System.out.println("Diciembre tiene 31 dias"); break;
+            default : System.out.println("Este valor no es valido"); break;
         }
     }
 }
