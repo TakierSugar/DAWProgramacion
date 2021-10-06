65b3f464f1377b9f2eb4c0c9ec4e6753d96229e6
Alejandro <takiersugar@gmail.com>
2021-10-06 09:51:36 +0200

    Ejemplo11 separados los espacios en blanco

--- Ejemplo11.java
+++ Ejemplo11.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 public class Ejemplo11 {
     public static  void main (String argv[]){
+
+        //Declaración de variables
         double A;
         double B;
-
         Scanner reader = new Scanner(System.in);
-
+        
+        //Empieza el programa
         System.out.println("Inserta número:");
         A = reader.nextInt();
+
         System.out.println("Inserta número:");
         B = reader.nextInt();
+
         reader.close();
 
         if (A > B){
-        System.out.println(B);
-        System.out.println(A);
+            System.out.println(B);
+            System.out.println(A);
         }else{
-        System.out.println(A);
-        System.out.println(B);    
+            System.out.println(A);
+            System.out.println(B);    
         }
     }
 }
