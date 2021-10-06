5bc06a49aa74accbb60c326bd0c4236d8e79a678
Alejandro <takiersugar@gmail.com>
2021-10-06 10:05:53 +0200

    11 cambiado A y B por X e Y

--- Ejemplo11.java
+++ Ejemplo11.java
@@ -3,25 +3,23 @@ public class Ejemplo11 {
     public static  void main (String argv[]){
 
         //Declaración de variables
-        double A;
-        double B;
+        double x;
+        double y;
         Scanner reader = new Scanner(System.in);
         
         //Empieza el programa
         System.out.println("Inserta número:");
-        A = reader.nextInt();
+        x = reader.nextInt();
 
         System.out.println("Inserta número:");
-        B = reader.nextInt();
+        y = reader.nextInt();
 
         reader.close();
 
-        if (A > B){
-            System.out.println(B);
-            System.out.println(A);
+        if (x > y){
+            System.out.println(y + " " + x);
         }else{
-            System.out.println(A);
-            System.out.println(B);    
+            System.out.println(x + " " + y);
         }
     }
 }
