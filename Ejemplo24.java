baa42c439c7e181719e527bbc3b98c4d64d379b5
Alejandro <takiersugar@gmail.com>
2021-10-08 09:43:46 +0200

    24 hecho

---
+++ Ejemplo24.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ejemplo24 {
+    
+    public static void main(String[] args) {
+        
+        //Declaración de variables
+        double nota, nota2 = 0, diez = 0, numero = 0;
+        Scanner reader = new Scanner (System.in);
+        //Empieza el programa
+
+        do  {
+            System.out.println("Dame una nota:");
+            nota = reader.nextDouble();
+
+            if (nota == 10) {
+
+            diez = diez + 1;
+            }
+            numero = numero + 1;
+            nota2 = nota2 + nota;
+        }while (nota != -1);
+        reader.close();
+        System.out.println("Hay" + " " + diez + " " + "dieces");
+        System.out.println("la nota media es" + " " + (nota2)/numero);
+    }
+}
