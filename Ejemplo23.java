a304d44409bd76f2948a256d80ef9911907ef3f1
Alejandro <takiersugar@gmail.com>
2021-10-08 09:43:37 +0200

    23 hecho

---
+++ Ejemplo23.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ejemplo23 {
+    
+    public static void main(String[] args) {
+        
+        //Declaración de variables
+        int numero, positivos = 0 ;
+        Scanner reader = new Scanner (System.in);
+        //Empieza el programa
+
+        do  {
+            System.out.println("Dame un número:");
+            numero = reader.nextInt();
+
+            if (numero > 0) {
+
+            positivos = positivos + 1;
+            }
+
+        }while (numero != 0);
+        reader.close();
+        System.out.println("Hay" + " " + positivos + " " + "números positivos");
+    }
+}
