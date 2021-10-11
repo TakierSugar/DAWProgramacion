f04c31d83c3731dc9682cdb8a8e8420dec893d14
Alejandro <takiersugar@gmail.com>
2021-10-11 09:06:19 +0200

    Cambio el 17 a solo 3 frases

--- Ejemplo17.java
+++ Ejemplo17.java
@@ -5,27 +5,41 @@ public class Ejemplo17 {
     public static void main(String[] args) {
         
         //Declaración de variables
-        int mes = 0;
+        byte mes = 0;
         Scanner reader = new Scanner(System.in);
 
         //Empieza el programa
-        System.out.println("introduce el mes: ");
-        mes = reader.nextInt();
+        System.out.println("introduce el número del mes: ");
+        mes = reader.nextByte();
         reader.close();
+
+            
+
         switch (mes){
-            case 1 : System.out.println("Enero tiene 31 dias"); break;
-            case 2 : System.out.println("Febrero tiene 28 dias o 29 si es año bisiesto"); break;
-            case 3 : System.out.println("Marzo tiene 31 dias"); break;
-            case 4 : System.out.println("Abril tiene 30 dias"); break;
-            case 5 : System.out.println("Mayo tiene 31 dias"); break;
-            case 6 : System.out.println("Junio tiene 30 dias"); break;
-            case 7 : System.out.println("Julio tiene 31 dias"); break;
-            case 8 : System.out.println("Agosto tiene 31 dias"); break;
-            case 9 : System.out.println("Septiembre tiene 30 dias"); break;
-            case 10 : System.out.println("Octubre tiene 31 dias"); break;
-            case 11 : System.out.println("Noviembre tiene 30 dias"); break;
-            case 12 : System.out.println("Diciembre tiene 31 dias"); break;
-            default : System.out.println("Este valor no es valido"); break;
+            case 1 :
+            case 3 :
+            case 5 : 
+            case 7 : 
+            case 8 :
+            case 10 : 
+            case 12 :
+            System.out.println("tiene 31 dias"); break;
+
+            case 2 :
+            System.out.println("tiene 28 dias o 29 si es año bisiesto"); break;
+
+            case 4 :
+            case 6 :
+            case 9 :
+            case 11 : 
+            System.out.println("tiene 30 dias"); break;
+            
+            default : System.out.println("Este valor no es valido");
         }
     }
 }
+
+
+//1 3 5 7 8 10 12 tienen 31 dias
+//2 tiene 28 dias
+// 4 6 9 11 tienen 30 dias
