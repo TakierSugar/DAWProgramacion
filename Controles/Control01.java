7b55a32e51b8e8e641de7dc0f7b8446c89c6c1c8
Alejandro <takiersugar@gmail.com>
2021-10-13 10:02:18 +0200

    corregido Control01

--- Controles/Control01.java
+++ Controles/Control01.java
@@ -3,30 +3,29 @@ import java.util.Scanner;
 
     public class Control01 {
         
-        public static void main(String[] args) {
+    public static void main(String[] args) {
             
         
-    //Declaracion de variables
-    int numero;
-    double longitud;
-    Scanner reader = new Scanner(System.in);
+        //Declaracion de variables
+        int numero;
+        double longitud;
+        Scanner reader = new Scanner(System.in);
 
-    //Empieza el programa
-    System.out.println("Dime el número de lados del polígono:");
-    numero = reader.nextInt();
+        //Empieza el programa
+        System.out.println("Dime el número de lados del polígono:");
+        numero = reader.nextInt();
 
-    if ((numero == 3)||(numero==4)){
+        if ((numero == 3)||(numero==4)){
 
-        System.out.println("Dime la longitud del lado del polígono:");
-        longitud = reader.nextDouble();
-        reader.close();
-        if (numero == 3){
-            System.out.println("El perímetro del triángulo es " + (longitud*numero));
-        }else if (numero == 4){
-            System.out.println("El perímetro del cuadrado es " + (longitud*numero));
+            System.out.println("Dime la longitud del lado del polígono:");
+            longitud = reader.nextDouble();
+            reader.close();
+            
+            if (numero == 3){
+                System.out.println("El perímetro del triángulo es " + (longitud*numero));
+            }else if (numero == 4){
+                System.out.println("El perímetro del cuadrado es " + (longitud*numero));
+            }
         }
-    }else{
     }
-    
-}
 }
