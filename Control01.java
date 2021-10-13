d8f9e7fd4b980e0351da542d247271888c24231d
Alejandro <takiersugar@gmail.com>
2021-10-13 09:23:56 +0200

    Hecho Control01

---
+++ Control01.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+    public class Control01 {
+        
+        public static void main(String[] args) {
+            
+        
+    //Declaracion de variables
+    int numero;
+    double longitud;
+    Scanner reader = new Scanner(System.in);
+
+    //Empieza el programa
+    System.out.println("Dime el número de lados del polígono:");
+    numero = reader.nextInt();
+
+    if ((numero == 3)||(numero==4)){
+
+        System.out.println("Dime la longitud del lado del polígono:");
+        longitud = reader.nextDouble();
+        if (numero == 3){
+            System.out.println("El perímetro del triángulo es " + (longitud*numero));
+        }else if (numero == 4){
+            System.out.println("El perímetro del cuadrado es " + (longitud*numero));
+        }
+    }else{ System.out.println("Este numero de lados es invalido");
+
+    }
+    reader.close();
+}
+}
