ff2195cd65db983c40fdd8e93271963b02a64d13
Alejandro <takiersugar@gmail.com>
2021-10-15 10:41:50 +0200

    While hecho

---
+++ While.java
@@ -0,0 +1,23 @@
+public class While {
+
+    public static void main(String[] args) {
+
+        int i=0;
+
+        for (i = 0; i < 10; i++){
+        System.out.println("Hola: " + i);
+        }
+
+        i = 0;
+        while(i < 10){
+            System.out.println("Hola: " + i);
+            i++;
+        }
+
+        i = 0;
+        do{
+            System.out.println("Hola: " + i);
+            i++;
+        }while (i < 10);
+    }
+}
