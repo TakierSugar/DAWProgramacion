3e1ed9531b605dabf8e45068bb760471af0cf358
Alejandro <takiersugar@gmail.com>
2021-10-18 09:26:11 +0200

    Puestos espacios en el 20

--- Ejemplo20.java
+++ Ejemplo20.java
@@ -13,13 +13,13 @@ public class Ejemplo20 {
         reader.close();
 
         if (precio < 6){
-            System.out.println("El articulo cuesta:" + " " + precio);
+            System.out.println("El articulo cuesta: " + precio);
         }else if (precio < 60){ 
             descuento = 0.95;
-            System.out.println("Tras recibir un descuento del 5% el articulo cuesta:" + " " + (precio * descuento) + "€");
+            System.out.println("Tras recibir un descuento del 5% el articulo cuesta: " + (precio * descuento) + "€");
         }else if (precio >= 60){ 
             descuento = 0.90;
-            System.out.println("Tras recibir un descuento del 10% el articulo cuesta:" + " " + (precio * descuento) + "€");
+            System.out.println("Tras recibir un descuento del 10% el articulo cuesta: " + (precio * descuento) + "€");
         }
     }
 }
