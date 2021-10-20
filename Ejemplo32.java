fec10955d0d45585071dd685de0cc759bdc9649a
Alejandro <takiersugar@gmail.com>
2021-10-20 08:25:25 +0200

    hecho 32

---
+++ Ejemplo32.java
@@ -0,0 +1,43 @@
+public class Ejemplo32 {
+    
+
+    public static void main(String[] args) {
+    
+        // declares an array of integers
+
+        int[] anArray;
+
+        // allocates memory for 10 integers
+
+        anArray = new int[10];
+
+        // initialize first element
+
+        anArray[0] = 13;
+
+        anArray[1] = 56;
+
+        anArray[2] = 37;
+
+        anArray[3] = 49;
+
+        anArray[4] = 47;
+
+        anArray[5] = 55;
+
+        anArray[6] = 38;
+
+        anArray[7] = 33;
+
+        anArray[8] = 544;
+
+        anArray[9] = 44;
+
+
+        for(int i=9; i>=0; i--){
+
+            
+            System.out.println("Elements at index " + i + ": " +anArray[i]);
+        }
+    }
+}
