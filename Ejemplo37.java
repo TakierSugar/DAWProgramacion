0a0b17120a68290b9fca890c676d9526a8010097
Alejandro <takiersugar@gmail.com>
2021-10-22 09:31:11 +0200

    Arreglo 37

--- Ejemplo37.java
+++ Ejemplo37.java
@@ -6,7 +6,7 @@ public class Ejemplo37 {
 
         //Declaro variables
         int[] numero = {1,2,3,4,5,6,7,8,9,10};
-        int menor = 0;
+        int menor = 0, mayor = 0;
         Scanner reader = new Scanner (System.in);
 
         //Empieza el programa
@@ -14,13 +14,16 @@ public class Ejemplo37 {
 
             System.out.println("Dame diez numeros: (llevas "+ (valor-1) +")");
             valor = reader.nextInt();
-                if (menor == 0){ menor = valor;
+                if (menor == 0){ menor = valor;mayor = valor;
                 }
             if (valor<menor){ 
                 menor=valor;
             }
+            if (valor>mayor){ 
+                mayor=valor;
+            }
         }
         reader.close();
-        System.out.println("El menor número es: " + menor);
+        System.out.println("El menor número es: " + menor + " y el mayor número es: " + mayor);
     }
 }
