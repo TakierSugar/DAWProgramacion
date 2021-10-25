a4f72626192df303375111e696138ddf01bd24ae
Alejandro <takiersugar@gmail.com>
2021-10-25 09:59:53 +0200

    40 hecho

---
+++ Ejemplo40.java
@@ -0,0 +1,18 @@
+public class Ejemplo40 {
+    
+    public static void main(String[] args) {
+    
+        //variables 
+        int n1 = 1, n2 = 1, t;
+        String lista = n1 + ", " + n2;
+
+        //inicia el programa
+        for(int i=3; i <= 40; i++){
+            t = n1 + n2;
+            lista = lista + ", " + t;
+            n1 = n2;
+            n2 = t;
+        }
+        System.out.println(lista);
+    }
+}
