5ce13b976f8c1d22edfc3049ae959a1a46ec304f
Alejandro <takiersugar@gmail.com>
2021-10-27 08:59:19 +0200

    ArgumentosMain

--- ArgumentosMain.java
+++ ArgumentosMain.java
@@ -1,8 +1,11 @@
 public class ArgumentosMain{
-
     public static void main(String[] args) {
-        for () 
+        System.out.println("=========================================="); 
+        System.out.println("Argumentos: " + args.length);
+        System.out.println("=========================================="); 
+        for (int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+        System.out.println("-------------------------------------------"); 
     }
-
-
 }
