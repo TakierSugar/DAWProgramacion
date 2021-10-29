c0a7ebc0f4abaf5174ebee7d57e7b460e2a8c223
Alejandro <takiersugar@gmail.com>
2021-10-29 08:19:50 +0200

    Mates

--- Tema2/Mates.java
+++ Tema2/Mates.java
@@ -1,15 +1,5 @@
 public class Mates {
 
-    public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            Integer.parseInt(args[i]);
-        
-        System.out.println(suma(x, y));
-        System.out.println(resta(x, y));
-        System.out.println(multiclicacion(x, y));
-        System.out.println(division(x, y));
-    }
-
     public static double suma(double x, double y){
         return x + y;
     }
@@ -18,12 +8,15 @@ public class Mates {
         return x - y;
     }
 
-    public static double multiclicacion(double x, double y){
+    public static double multiplicacion(double x, double y){
         return x * y;
     }
 
     public static double division(double x, double y){
         return x / y;
     }
-    
+
+    public static double areaCirculo(double radio){
+        return Math.PI * radio * radio;
+    }
 }
