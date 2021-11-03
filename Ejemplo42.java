208c064f7de4c38d81ff037950b4f270e7e611b6
Alejandro <takiersugar@gmail.com>
2021-11-03 09:42:57 +0100

    42 hecho

---
+++ Ejemplo42.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejemplo42 {
+    public static void main(String[] args) {
+    
+        //variables
+        int numero1 = 0, numero2 = 0, multiplicacion = numero1;
+        Scanner reader = new Scanner (System.in);
+
+        //empieza el programa
+        System.out.println("Dime el primer número");
+        numero1 = reader.nextInt(); 
+        System.out.println("Dime el otro número");
+        numero2 = reader.nextInt();
+        reader.close(); 
+
+        for (int i = 0 ; i < numero2 ; i++){
+            multiplicacion=multiplicacion+numero1;
+        } 
+        System.out.println(multiplicacion);
+    }  
+} 
