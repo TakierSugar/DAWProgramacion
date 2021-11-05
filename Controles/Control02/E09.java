49e2e49dee74527fe577c2f5521948bd76a40f55
Alejandro <takiersugar@gmail.com>
2021-11-05 10:32:07 +0100

    E09 resuelto

---
+++ Controles/Control02/E09.java
@@ -0,0 +1,24 @@
+package Controles.Control02;
+
+import java.util.Scanner;
+
+public class E09 {
+    public static void main(String[] args) {
+        //variables
+        double Nc, C1, C2, C3, NF = 60, NL;
+        Scanner reader = new Scanner(System.in);
+
+        //programa
+        System.out.println("Ingrese nota certamen 1: ");
+        C1 = reader.nextInt();
+        System.out.println("Ingrese nota certamen 2: ");
+        C2 = reader.nextInt();
+        System.out.println("Ingrese nota laboratorio: ");
+        NL = reader.nextInt();
+
+        Nc = ((NF-(NL*0.3))/0.7);
+        C3 = (Nc*3)- C1 - C2;
+
+        System.out.println(C3);
+    }
+}
