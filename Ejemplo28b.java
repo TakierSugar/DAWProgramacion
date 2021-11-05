d7bdcc3c2430a5f4ed519744d592f5a739eec0c0
Alejandro <takiersugar@gmail.com>
2021-11-05 08:08:08 +0100

    28b hecho

--- Ejemplo28b.java
+++ Ejemplo28b.java
@@ -11,16 +11,16 @@ public class Ejemplo28b {
         //Empieza el progama
         System.out.println("Dame un número");
         numero = reader.nextInt();
-
+        reader.close();
         for (int i = 1; i < numero; i++){
             if (numero%i==0){
                 contador = contador + 1;
             }
         }
         if (contador == 2){
-            System.out.println(numero + " es un número primo");
-        }else{
             System.out.println(numero + " no es un número primo");
+        }else{
+            System.out.println(numero + " es un número primo");
         }
     }
 }
