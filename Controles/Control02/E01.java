5c9082b76843eba6576950e577229e13900aac74
Alejandro <takiersugar@gmail.com>
2021-11-05 09:05:41 +0100

    E01 resuelto

---
+++ Controles/Control02/E01.java
@@ -0,0 +1,19 @@
+package Controles.Control02;
+
+import java.util.Scanner;
+
+public class E01 {
+
+    public static void main(String[] args) {
+        
+        //variables
+        String nombre;
+        Scanner reader = new Scanner(System.in);
+
+        //comienza el programa
+        System.out.println("Ingrese su nombre: ");
+        nombre = reader.nextLine();
+        System.out.println("Hola, " + nombre);
+    }
+    
+}
