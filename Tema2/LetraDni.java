77e2c958fb026daaad54b7a8a3b489ab5309e514
Alejandro <takiersugar@gmail.com>
2021-11-10 10:29:20 +0100

    LetraDni Hecha

---
+++ Tema2/LetraDni.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class LetraDni {
+
+    public static String letraDni(){
+
+        int DNI, resto;
+        String[] Letra = new String[23];
+
+        Letra[0] = "T";
+
+        Letra[1] = "R";
+
+        Letra[2] = "W";
+
+        Letra[3] = "A";
+
+        Letra[4] = "G";
+
+        Letra[5] = "M";
+
+        Letra[6] = "Y";
+
+        Letra[7] = "F";
+
+        Letra[8] = "P";
+
+        Letra[9] = "D";
+
+        Letra[10] = "X";
+
+        Letra[11] = "B";
+
+        Letra[12] = "N";
+
+        Letra[13] = "J";
+
+        Letra[14] = "Z";
+
+        Letra[15] = "S";
+
+        Letra[16] = "Q";
+
+        Letra[17] = "V";
+
+        Letra[18] = "H";
+
+        Letra[19] = "L";
+
+        Letra[20] = "C";
+
+        Letra[21] = "K";
+
+        Letra[22] = "E";
+
+        Scanner reader = new Scanner (System.in);
+        System.out.println("Dime un DNI sin la letra");
+        
+        DNI = reader.nextInt();
+        reader.close();
+        resto = DNI%23;
+        return Letra[resto];
+    }
+}
---
+++ Tema2/LetraDniPrueba.java
@@ -0,0 +1,6 @@
+public class LetraDniPrueba {
+    public static void main(String[] args) {
+        String resultado = LetraDni.letraDni();
+        System.out.println(resultado);
+    }
+}
