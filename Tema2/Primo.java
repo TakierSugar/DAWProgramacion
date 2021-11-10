60f3c7fcbb6a2ebf4b0f80b22ca003b02a876cf1
Alejandro <takiersugar@gmail.com>
2021-11-10 10:40:09 +0100

    Primo hecho

---
+++ Tema2/Primo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Primo {
+    public static boolean esPrimo(){
+        
+        int numero, contador = 0;
+        Scanner reader = new Scanner (System.in);
+        boolean primo;
+        
+        System.out.println("Dame un número");
+        numero = reader.nextInt();
+        reader.close();
+        for (int i = 1; i < numero; i++){
+            if (numero%i==0){
+                contador = contador + 1;
+            }
+        }
+        if (contador == 2){primo = false;     
+        }else{ primo = true;
+        }
+        
+        if (primo = false){System.out.println(numero + " no es un número primo");
+        }else if(primo = true){System.out.println(numero + " es un número primo");
+        }
+        return primo;
+        
+    }
+}
---
+++ Tema2/PrimoPrueba.java
@@ -0,0 +1,6 @@
+public class PrimoPrueba {
+    public static void main(String[] args) {
+        boolean resultado = Primo.esPrimo();
+        System.out.println(resultado);
+    }
+}
