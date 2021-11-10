168c148a50fd72cd48f837654d19c03a8e272681
Alejandro <takiersugar@gmail.com>
2021-11-10 09:25:30 +0100

    EsDivisible hecho

---
+++ Tema2/EsDivisible.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EsDivisible {
+    public static boolean esDivisible(){
+
+        //variables
+        int dividendo, divisor;
+        Scanner reader = new Scanner(System.in);
+        boolean resultado = false;
+
+        System.out.println("Dame el dividendo");
+        dividendo = reader.nextInt();
+        System.out.println("Dame el divisor");
+        divisor = reader.nextInt();
+        reader.close();
+        if (dividendo%divisor == 0){
+            resultado = true;
+        }else if (dividendo%divisor != 0){ resultado = false;
+        }
+        return resultado;
+    }
+}
---
+++ Tema2/EsDivisiblePrueba.java
@@ -0,0 +1,6 @@
+public class EsDivisiblePrueba {
+    public static void main(String[] args) {
+        boolean resultado = EsDivisible.esDivisible();
+        System.out.println(resultado);
+    }
+}
