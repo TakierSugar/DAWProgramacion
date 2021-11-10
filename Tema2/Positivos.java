9d2de04938b9dca5725100907714ab740bd87f1d
Alejandro <takiersugar@gmail.com>
2021-11-10 10:20:32 +0100

    Positivos hecho

---
+++ Tema2/Positivos.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Positivos {
+    public static int positivos(){
+        int positivos = 0;
+        int[] numero = new int[8];
+        Scanner reader = new Scanner(System.in);
+
+        for(int i = 0; i <7; i++){
+            System.out.println("Dame un número");
+            numero[i] = reader.nextInt();
+            if (numero[i]>0) positivos++;
+        }
+        reader.close();
+        return positivos;
+    } 
+}
---
+++ Tema2/PositivosPrueba.java
@@ -0,0 +1,7 @@
+public class PositivosPrueba {
+    public static void main(String[] args) {
+        double resultado = Positivos.positivos();
+        System.out.println(resultado);
+    }
+}
+
