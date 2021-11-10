5a20735832f7e653c3433aadc2d95317acb28d40
Alejandro <takiersugar@gmail.com>
2021-11-10 09:33:22 +0100

    EsTriangulo hecho

---
+++ Tema2/EsTriangulo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EsTriangulo {
+    public static boolean esTriangulo(){
+
+        //variables
+        int a,b,c;
+        Scanner reader = new Scanner(System.in);
+        boolean resultado = false;
+
+        System.out.println("Dame a");
+        a = reader.nextInt();
+        System.out.println("Dame b");
+        b = reader.nextInt();
+        System.out.println("Dame c");
+        c = reader.nextInt();
+        reader.close();
+
+        if (a+b < c || a+c < b || b+c < a){resultado = false;
+        }else{
+            resultado = true;
+        }
+        return resultado;
+    }
+}
---
+++ Tema2/EsTrianguloPrueba.java
@@ -0,0 +1,7 @@
+public class EsTrianguloPrueba {
+    public static void main(String[] args) {
+        boolean resultado = EsTriangulo.esTriangulo();
+        System.out.println(resultado);
+    }
+}
+
