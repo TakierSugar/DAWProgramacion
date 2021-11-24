ec5d0d7f642a330b8327ac86f8e425c71f50ed32
Alejandro <takiersugar@gmail.com>
2021-11-24 09:57:34 +0100

    SumaIgual hecho

---
+++ Tema3/SumaIgual.java
@@ -0,0 +1,25 @@
+package Tema3;
+
+import java.util.Scanner;
+
+public class SumaIgual {
+    public static void main(String[] args) {
+        //variables
+        int[] numeros = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        int NumeroQuerido;
+        Scanner lector = new Scanner(System.in);
+
+        //programa
+        System.out.print("Dame el valor de la suma: ");
+        NumeroQuerido = lector.nextInt();
+        lector.close();
+
+        for (int i = 0; i <= numeros.length; i++){
+            for (int j = 0; j < numeros.length; j++){
+                if( i + j == NumeroQuerido){
+                    System.out.println(i + " + " + j + " = " + NumeroQuerido);
+                }
+            }
+        }
+    }
+}
