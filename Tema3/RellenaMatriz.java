6c2dd81d0cdbf15abe3e449b4da68fc46ec3d692
Alejandro <takiersugar@gmail.com>
2021-11-29 09:04:02 +0100

    RellanaMatriz

--- Tema3/RellenaMatriz.java
+++ Tema3/RellenaMatriz.java
@@ -8,8 +8,8 @@ public class RellenaMatriz {
             System.out.println();
             for (int j = 0; j < matriz.length; j++){
                 double aleatorio = Math.random();
-                if (aleatorio<0.5){System.out.print("0");}
-                else if (aleatorio>=0.5){System.out.print("1");}
+                if (aleatorio<0.5){System.out.print("0 ");}
+                else if (aleatorio>=0.5){System.out.print("1 ");}
             }
         }
         System.out.println();
