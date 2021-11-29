93c7eb380ca23ef4ea2179badc32e8320e17dac1
Alejandro <takiersugar@gmail.com>
2021-11-29 08:54:26 +0100

    RellenaMatriz

---
+++ Tema3/RellenaMatriz.java
@@ -0,0 +1,17 @@
+package Tema3;
+
+public class RellenaMatriz {
+    public static void main(String[] args) {
+        int[][] matriz = new int [10][10];
+
+        for (int i = 0; i < matriz.length; i++){
+            System.out.println();
+            for (int j = 0; j < matriz.length; j++){
+                double aleatorio = Math.random();
+                if (aleatorio<0.5){System.out.print("0");}
+                else if (aleatorio>=0.5){System.out.print("1");}
+            }
+        }
+        System.out.println();
+    }   
+}
