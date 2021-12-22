59b9773626c0c016ddebdb61adaf8e94bda391b6
Alejandro <takiersugar@gmail.com>
2021-12-22 10:07:58 +0100

    Longitud.java acabado

---
+++ Strings/Longitud.java
@@ -0,0 +1,21 @@
+public class Longitud {
+    
+    public static int medirLongitud(String[] palabras, int longitud) {
+        int cuantas = 0;
+        for (String palabra : palabras) {
+            if (palabra.length() == longitud) {
+                cuantas++;
+            }
+        }
+        return cuantas;
+    }
+    public static void main(String[] args) {
+        String cadena = Utilidades.leerCadena("Introduce un texto");
+        String palabras[] = Utilidades.dividirEnPalabras(cadena);
+        int longitud = Utilidades.leerEntero("¿De que longitud son las palabras?");
+
+        System.out.printf("La cadena contiene %d palabras de longitud %d", medirLongitud(palabras, longitud), longitud);
+    }
+
+}
+  
