564cec35a5c533b76b9a31f6ff4bedec452e2fa1
Alejandro <takiersugar@gmail.com>
2022-01-26 08:51:23 +0100

    Añado algunas pruebas de funcionamiento de TicketMachine2

--- POO/TicketMachineTest2.java
+++ POO/TicketMachineTest2.java
@@ -4,8 +4,11 @@ public class TicketMachineTest2 {
     public static void main(String[] args) {
         TicketMachine2 Ticket;
         Ticket = new TicketMachine2(200);
-        Ticket.insertMoney(2000);
-        Ticket.setDiscount(200);    
+        Ticket.insertMoney(10);
+        Ticket.setDiscount(200);  
+        Ticket.printTicket2();
+        Ticket.insertMoney(200);
+        Ticket.printTicket2();  
     }
     
 }
