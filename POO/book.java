35af8e4a28af0907e6eb87e0aa8d2022b4757a5d
Alejandro <takiersugar@gmail.com>
2022-01-26 10:17:41 +0100

    book acabado

---
+++ POO/book.java
@@ -0,0 +1,81 @@
+package POO;
+
+class Book
+{
+    // The fields.
+    private String author;
+    private String title;
+    private int pages;
+    private String refNumber;
+    private int borrowed;
+    private boolean courseText;
+
+    /**
+     * Set the author and title fields when this object
+     * is constructed.
+     */
+    public Book(String bookAuthor, String bookTitle, int bookPages, int bookBorrowed, boolean bookCourseText)
+    {
+        author = bookAuthor;
+        title = bookTitle;
+        pages = bookPages;
+        refNumber = "";
+        borrowed = bookBorrowed;
+        courseText = bookCourseText;
+    }
+
+    // Add the methods here ...
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPages(){
+        return pages;
+    }
+
+    public String getRefNumber(){
+        return refNumber;
+    }
+
+    public int getBorrowed(){
+        return borrowed;
+    }
+
+    public String printAuthor() {
+        System.out.println(author);
+        return author;
+    }
+
+    public String printTitle() {
+        System.out.println(title);
+        return title;
+    }
+
+    public String printDetails() {
+        String details = author + ", " + title + ", " + pages + ", " + refNumber + ", " + borrowed + ", " + courseText;
+        System.out.println(details);
+        return details;
+    }
+
+    public void  setRefNumber(String ref) {
+        if (ref.length() < 3) 
+        System.out.println("error");
+        else
+        refNumber = ref;
+    }
+
+    public int borrow(int borrowed) {
+        this.borrowed = borrowed++;
+        return this.borrowed;
+    }
+
+    public boolean isCourseText() {
+        return courseText;
+    }
+
+}
---
+++ POO/bookTest.java
@@ -0,0 +1,9 @@
+package POO;
+
+public class bookTest {
+    public static void main(String[] args) {
+        Book B1;
+        B1 = new Book("Jose", "Tortillaaaaaaaas", 0, 12, true);
+        B1.printDetails();
+    }
+}
