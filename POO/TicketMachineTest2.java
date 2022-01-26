d3dd863d7fdb42b4bbc238f194b7fa00f5d46596
Alejandro <takiersugar@gmail.com>
2022-01-26 08:42:33 +0100

    TicketMachine2 completo

---
+++ POO/TicketMachineTest2.java
@@ -0,0 +1,11 @@
+package POO;
+
+public class TicketMachineTest2 {
+    public static void main(String[] args) {
+        TicketMachine2 Ticket;
+        Ticket = new TicketMachine2(200);
+        Ticket.insertMoney(2000);
+        Ticket.setDiscount(200);    
+    }
+    
+}
