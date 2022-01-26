ddaf0d236f63e57c858ad1e3897ebfdb7ea0b06b
Alejandro <takiersugar@gmail.com>
2022-01-26 10:33:28 +0100

    Quito borrowed del constructor

--- POO/book.java
+++ POO/book.java
@@ -14,13 +14,12 @@ class Book
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle, int bookPages, int bookBorrowed, boolean bookCourseText)
+    public Book(String bookAuthor, String bookTitle, int bookPages, boolean bookCourseText)
     {
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
         refNumber = "";
-        borrowed = bookBorrowed;
         courseText = bookCourseText;
     }
 
--- POO/bookTest.java
+++ POO/bookTest.java
@@ -3,7 +3,7 @@ package POO;
 public class bookTest {
     public static void main(String[] args) {
         Book B1;
-        B1 = new Book("Jose", "Tortillaaaaaaaas", 0, 12, true);
+        B1 = new Book("Jose", "Tortillaaaaaaaas", 0, true);
         B1.printDetails();
         B1.setRefNumber("Esto deberia ser correcto");
         B1.printDetails();
