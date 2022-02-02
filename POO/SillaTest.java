2c78b51fb21f8c566d91b8ea32c070e3296e3b3a
Alejandro <takiersugar@gmail.com>
2022-02-02 08:49:04 +0100

    Cambio Sillas, ahora si funciona

--- POO/SillaTest.java
+++ POO/SillaTest.java
@@ -3,10 +3,11 @@ package POO;
 public class SillaTest {
 
     public static void main(String[] args) {
-    
-        Silla s1 = new Silla();
-        Silla s2 = new Silla();
 
-        s1.setComodidad(4);
+        Silla [] sillas = new Silla[100];
+        for (int i = 0; i < 100; i++) {            
+            sillas[i] = new Silla();
+        }
+        
     }
 }
