6966fc4b1421484f38a7b0bfba1bdd2e5cca6299
Alejandro <takiersugar@gmail.com>
2022-02-02 08:26:43 +0100

    Cambio en el nombre del sujeto

--- POO/mailSystem/MailTest.java
+++ POO/mailSystem/MailTest.java
@@ -7,8 +7,8 @@ public class MailTest {
     servidor = new MailServer();
     MailClient Cliente1, Cliente2;
     Cliente1 = new MailClient(servidor,"Paco");
-    Cliente2 = new MailClient(servidor, "Mini-Paco");
-    Cliente1.sendMailItem("Mini-Paco", "Estoy probando si funciona el programa", "Prueba");
+    Cliente2 = new MailClient(servidor, "Juan");
+    Cliente1.sendMailItem("Juan", "Estoy probando si funciona el programa", "Prueba");
     Cliente2.sendMailItem("Paco", "OK, funciona perfectamente", "Reply to Paco");
     System.out.println(servidor.howManyMailItems("Paco"));
     Cliente2.printNextMailItem();
